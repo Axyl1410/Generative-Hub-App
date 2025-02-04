c957d35c86c64348ebd99dc923becaeedbab7f7d
Alex <truonggiang190689@gmail.com>
2025-02-05 05:21:55 +0700

    Enhance add-address API: improve error handling, implement CORS support, and standardize JSON responses

--- app/api/add-address/route.ts
+++ app/api/add-address/route.ts
@@ -6,24 +6,46 @@ export async function POST(request: Request) {
     const { username, address } = await request.json();
 
     if (!username || !address) {
-      return NextResponse.json(
-        {
-          error: "Username and address are required",
-        },
-        { status: 400 }
+      return new NextResponse(
+        JSON.stringify({ error: "Username and address are required" }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
       );
     }
 
     await addAddressToUser(username, address);
 
-    return NextResponse.json(
-      { message: "Address added successfully" },
-      { status: 200 }
+    return new NextResponse(
+      JSON.stringify({ message: "Address added successfully" }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
     );
   } catch (error) {
-    return NextResponse.json(
-      { error: `Internal Server Error ${error}` },
-      { status: 500 }
+    console.error("Error adding address:", error);
+
+    return new NextResponse(
+      JSON.stringify({ error: `Internal Server Error: ${error}` }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
     );
   }
 }
+
+export const OPTIONS = async (request: Request) => {
+  const allowedOrigins = process.env.ALLOWED_ORIGINS
+    ? process.env.ALLOWED_ORIGINS.split(",")
+    : ["http://localhost:3000"];
+  const origin = request.headers.get("origin") || "*";
+
+  if (allowedOrigins.includes(origin) || origin === "*") {
+    return new NextResponse(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Methods": "POST, OPTIONS", // Include OPTIONS
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  } else {
+    return new NextResponse(null, {
+      status: 403,
+    });
+  }
+};
